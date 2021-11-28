02ce7d4dc4ed5e4d5374247c51931558c5501720
devhulk <gry2big93@gmail.com>
2021-11-28 18:24:29 -0500

    added rg output

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
+output "resource_group_name" {
+    value = azurerm_resource_group.example.name
+} 
+
+output "resource_group_id" {
+    value = azurerm_resource_group.example.id
+} 
+
 output "vnet_id" {
     value = azurerm_virtual_network.example.id
 }
