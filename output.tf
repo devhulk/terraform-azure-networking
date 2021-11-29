5e6e40c56799b04267ac26ad9dc6f43cb67c3173
devhulk <gry2big93@gmail.com>
2021-11-28 19:25:03 -0500

    added rg location

--- output.tf
+++ output.tf
@@ -6,6 +6,10 @@ output "resource_group_id" {
     value = azurerm_resource_group.example.id
 } 
 
+output "resource_group_location" {
+    value = azurerm_resource_group.example.location
+} 
+
 output "vnet_id" {
     value = azurerm_virtual_network.example.id
 }
